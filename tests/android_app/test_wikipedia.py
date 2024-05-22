03cd9f0964bbb74fed54b3f2d9f1605d23d0ad97
Aleksandr Mosyakin <aleks.mosyakin@gmail.com>
2024-05-22 21:08:17 +0300

    init commit

---
+++ tests/android_app/test_wikipedia.py
@@ -0,0 +1,32 @@
+import allure
+from allure_commons._allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+
+@allure.feature('Android')
+@allure.title('Поиск на сайте Wikipedia')
+def test_search():
+
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
+
+
+def test_open_page():
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Python')
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Python'))
+
+    with step('Open first result'):
+        results.first.click()
---
+++ tests/web/test_wikipedia.py
@@ -0,0 +1,17 @@
+import allure
+from selene import have, browser
+from allure import step
+
+
+@allure.feature('WEB')
+@allure.title('Поиск на сайте Wikipedia')
+def test_search():
+    browser.open('/')
+
+    with step('Type search'):
+        browser.element('#searchInput').type('Appium')
+
+    with step('Verify content found'):
+        results = browser.all('.suggestion-link')
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
